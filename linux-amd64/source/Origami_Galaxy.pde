e6dda3b9ff2813d0eeca30d601932859ae398744
AleksDimitrov <adimitrov@email.arizona.edu>
2023-05-21 16:19:51 -0700

    Re-exporting executables to include Java

---
+++ linux-amd64/source/Origami_Galaxy.pde
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1aebbbdb75e8fc8ed9734862293459f59b25293def0a4b438622eef866fcc4a3
+size 4701
---
+++ windows-amd64/source/Origami_Galaxy.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a24a23beb3464267654b8a22e7eb3e5cbc0b056c3d1ab9c5902f1ed71b911fb
+size 5501
